faa7b355870a3b908f9d1f5f643d54e66480079f
tracyyyxue <txue25@g.ucla.edu>
2025-08-22 21:31:09 -0700

    working example.py

---
+++ example.py
@@ -0,0 +1,24 @@
+def main():
+    import pandas as pd # Please install pandas and matplotlib before you run this example
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import scipy
+    import genomap as gp
+
+    data = pd.read_csv('./data/TM_data.csv', header=None)
+    colNum=33 # Column number of genomap
+    rowNum=33 # Row number of genomap
+
+    dataNorm=scipy.stats.zscore(data,axis=0,ddof=1) # Normalization of the data
+
+    genoMaps=gp.construct_genomap(dataNorm,rowNum,colNum,epsilon=0.0,num_iter=200) # Construction of genomaps
+
+    findI=genoMaps[10,:,:,:]
+
+    plt.figure(1) # Plot the first genomap
+    plt.imshow(findI, origin = 'lower',  extent = [0, 10, 0, 10], aspect = 1)
+    plt.title('Genomap of a cell from TM dataset')
+    plt.show()
+
+if __name__=='__main__':
+    main()
